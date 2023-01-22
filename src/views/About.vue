9f07ba1fc9639644d703b9abadb713f645726009
xmasuhai <xmasuhai@163.com>
2023-01-22 22:06:59 +0800

    :sparkles: 测试组合式api使用路由实例

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts" name="About">
 import {useUserStore} from "@/store/modules/userStore"
+import {useRouter, useRoute,} from 'vue-router'
 
 // 实例化使用userStore
 const users = useUserStore();
@@ -7,10 +8,26 @@ const users = useUserStore();
 // 存储数据
 users.SET_USERNAME("user is in About");
 
+// 使用路由
+const router = useRouter()
+const route = useRoute()
+const pushWithQuery = () => {
+  console.log('route', route.path, route.query)
+  router.push({
+    name: 'search',
+    query: {
+      ...route.query,
+    },
+    path: '/',
+  })
+}
+
 </script>
 
 <template>
   About!!! {{ users.username }}
+  <br/>
+  <button @click="pushWithQuery">Home</button>
 </template>
 
 <style lang="scss" scoped>
