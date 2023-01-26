8fa6e092dd2f99bc68144b9211b3cfd7e6202480
xmasuhai <xmasuhai@163.com>
2023-01-26 22:07:53 +0800

    💡 add comments 🔧 update vite.config.ts, add config options of unplugin-auto-import & unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -28,14 +28,40 @@ export default defineConfig({
             // default imports
             ['default', 'axios'], // import { default as axios } from 'axios',
           ],
+          /*
+          '@vueuse/core': [
+            // named imports
+            'useMouse', // import { useMouse } from '@vueuse/core',
+            // alias
+            ['useFetch', 'useMyFetch'], // import { useFetch as useMyFetch } from '@vueuse/core',
+          ],
+          */
+
         },
       ],
       // UI
       resolvers: [ElementPlusResolver()],
       dts: true,
     }),
+
+    // By default, this plugin will import components in the src/components path. (or customize it using the dirs option)
     Components({
+      dts: true, // enabled by default if `typescript` is installed
       resolvers: [ElementPlusResolver()],
+      deep: true, // search for subdirectories
+      directoryAsNamespace: true, // Allow subdirectories as namespace prefix for components
+
+      /*
+      Collapse same prefixes (camel-sensitive) of folders and components
+      to prevent duplication inside namespaced component name.
+      works when `directoryAsNamespace: true`
+      */
+      collapseSamePrefixes: true,
+      globalNamespaces: [], // Subdirectory paths for ignoring namespace prefixes.
+      // filters for transforming targets
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      exclude: [/[\\/]node_modules[\\/]/, /[\\/]\.git[\\/]/, /[\\/]\.nuxt[\\/]/],
+
     }),
   ],
   resolve: {
