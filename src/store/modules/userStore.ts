662e772eab1549c6687370aa5952ff7ab6dd1a8f
xmasuhai <xmasuhai@163.com>
2023-01-26 21:38:03 +0800

    💡 add comments 📌 lock deps 🔧 vite.config.ts ⬆️ update deps

--- src/store/modules/userStore.ts
+++ src/store/modules/userStore.ts
@@ -18,15 +18,22 @@ export const useUserStore = defineStore({
   // 数据持久化配置
   persist: {
     enabled: true, // 开启存储
-    // strategies: 指定存储位置以及存储的变量
-    // 该属性可以不写，不写的话默认是存储到sessionStorage里边
-    // 默认存储state里边的所有数据
+
+    /*
+    * strategies: 指定存储位置以及存储的变量
+    * 该属性可以不写，不写的话默认是存储到sessionStorage里边
+    * 默认存储state里边的所有数据
+    *
+    * */
     strategies: [
-      {storage: localStorage, paths: ['username']}
-      // storage 存储到哪里 sessionStorage/localStorage
-      // paths是数组，要存储缓存的变量，也可以写多个变量
-      // paths如果不写，那么默认存储就是state里边的所有数据
-    ]
+      {
+        // storage 存储到哪里 sessionStorage/localStorage
+        storage: localStorage,
+        // paths是数组，要存储缓存的变量，也可以写多个变量
+        // paths如果不写，那么默认存储就是state里边的所有数据
+        paths: ['username'],
+      },
+    ],
   },
   actions: {
     SET_USERNAME(payload: string) {
--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
           ],
         },
       ],
+      // UI
       resolvers: [ElementPlusResolver()],
       dts: true,
     }),
