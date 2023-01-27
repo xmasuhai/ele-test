cc91e2a1fab6b801e6aacf8162810b0380553213
xmasuhai <xmasuhai@163.com>
2023-01-27 21:58:55 +0800

    💄 登录组件头像样式

--- src/components/layout/HeaderMainFooter.vue
+++ src/components/layout/HeaderMainFooter.vue
@@ -3,15 +3,15 @@
 </script>
 
 <template>
-  <header class="header">
+  <el-header class="header">
     <slot name="header">header</slot>
-  </header>
-  <main class="main">
+  </el-header>
+  <el-main class="main">
     <slot>main</slot>
-  </main>
-  <footer class="footer">
+  </el-main>
+  <el-footer class="footer">
     <slot name="footer">footer</slot>
-  </footer>
+  </el-footer>
 </template>
 
 <style lang="scss" scoped>
@@ -22,20 +22,20 @@
   flex: 1 1 auto;
 }
 
-header.header {
+.header {
   height: 10vh;
   width: 100vw;
   @extend %place-center;
 }
 
-main.main {
+.main {
   height: 80vh;
   width: 80vw;
   max-width: 1280px;
   @extend %place-center;
 }
 
-footer.footer {
+.footer {
   height: 10vh;
   width: 100vw;
   @extend %place-center;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,79 +1,18 @@
 <script lang="ts" name="Login" setup>
-/* ref */
-// const count: Ref<number> = ref(0)
+import {Ref, ref} from 'vue'
+import avatarRound from '@/assets/images/avatar/img_avatar_male_round.png'
+import HeaderMainFooter from "@/components/layout/HeaderMainFooter"
 
-/* reactive */
-// interface Num {count?: number}
-// const state: Num = reactive<{count: number}>({ count: 0 })
-
-/* props */ // Syntax Limitations: https://vuejs.org/guide/typescript/composition-api.html#typing-component-props
-// const props = defineProps({ foo: { type: String, required: true },}) // runtime props option
-// const props = defineProps<{ msg: string }>() // generic type argument
-// cannot use both runtime and generic at the same time
-/* move the props types into a separate interface + Props Default Values */
-/*
-interface Props {
-  foo: string
-  bar?: number
-}
-const props = withDefaults(defineProps<Props>(), {...})
-*/
-// const { name, count = 100 } = defineProps<Props>() // vue@^3.2.25+ need set vite.config.js // https://vuejs.org/guide/extras/reactivity-transform.html#explicit-opt-in
-/* PropType */
-// import type { PropType } from 'vue'
-// interface Book { title: string }
-// const props = defineProps({ book: Object as PropType<Book> })
-
-/* emits */
-// const emit = defineEmits(['change', 'update']) // runtime
-// const emit = defineEmits<{
-//   (e: 'change', id: number): void
-//   (e: 'update', value: string): void
-// }>() // type-based
-
-/* methods */
-// const increment = () => {count.value++} // ref
-// const increment = () => {state.count++} // reactive
-// emit('change') // emits
-
-/* computed */
-// const doubleCount = computed<number>(() => {() => count.value * 2})
-
-/* watch */
-// watch(anyRef, async (new, old) => {}, { deep: true, immediate: true }) // single ref
-// watch(() => x.value + reactiveObj.count, (returnOfGetter) => {}, ) // getter // watch a property of a reactive object only by getter, deep by default
-// watch([x, () => y.value], ([newX, newY], returnOfGetter) => {}, ) // array of multiple sources
-// watchEffect(async () => { data.value = await fetch('...') }) // watch automatically, immediately by default
-/* using watchEffect, only properties accessed before the first await tick will be tracked */
-
-/* Event Handlers: <input type="text" @change="handleChange" /> */
-// const eventHandler = (e: Event) => {console.log((event.target as HTMLInputElement).value)}
-
-/*  Provide/Inject */
-// import type { InjectionKey } from 'vue'
-// const key = Symbol() as InjectionKey<string>
-// provide(key, 'foo') // providing non-string value will result in error
-// in other component: // const foo = inject(key) // type of foo: string | undefined
-
-/* Template Refs */
-/* html elements */
-// <input ref="el" />
-// const el = ref<HTMLInputElement | null>(null)
-// onMounted(() => { el.value?.focus() }) // use optional chaining or type guards
-/* components: invoke method expose by Child Comp */
-// const open = () => (isContentShown.value = true) // in Child Comp
-// defineExpose({ open })
-// import MyComp from './MyComp.vue' // in Parent Comp
-// const modal = ref<InstanceType<typeof MyModal> | null>(null)
-// const openModal = () => { modal.value?.open() }
-import HeaderMainFooter from "@/components/layout/HeaderMainFooter.vue";
+const avatarRoundImgSrc: Ref<string> = ref(avatarRound)
 </script>
 
 <template>
   <div class="login-container">
     <HeaderMainFooter>
       <div class="login-box">
-
+        <div class="avatar-box">
+          <img :src="avatarRoundImgSrc" alt="">
+        </div>
       </div>
     </HeaderMainFooter>
   </div>
@@ -93,6 +32,26 @@ import HeaderMainFooter from "@/components/layout/HeaderMainFooter.vue";
     height: 300px;
     background-color: #fff;
     border-radius: 10px;
+
+    .avatar-box {
+      width: 125px;
+      height: 125px;
+      border-radius: 50%;
+      padding: 10px;
+      box-shadow: 0 0 10px #ddd;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+
   }
 }
 </style>
